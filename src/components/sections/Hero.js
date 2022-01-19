018bf15e8f698b9da91fc71f31b0c447c9cabe1e
Vishwa <vishywashy03@gmail.com>
2022-01-19 17:25:08 -0500

    change title for landing page

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,11 +61,11 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              VVAMP Inc.
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Re-imagining the industry standard for recycling and reducing e-waste.
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
