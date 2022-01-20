10ddd88f314853fa89c13f9ed1f240780bb39c79
Vishwa <vishywashy03@gmail.com>
2022-01-19 22:17:02 -0500

    revert team name

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,7 +61,7 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Automated PCB Disassembler BLAH
+              Automated PCB Disassembler
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
