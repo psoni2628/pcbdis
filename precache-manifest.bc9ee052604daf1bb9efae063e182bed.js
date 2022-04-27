c431fec32730707ec5e6fbd28601d526f5bea43e
Vishwa <vishywashy03@gmail.com>
2022-04-27 11:44:39 -0400

    Updates

---
+++ precache-manifest.bc9ee052604daf1bb9efae063e182bed.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dbb7a2e6fa352de8aa82b45a03d1a0",
+    "url": "/pcbdis/index.html"
+  },
+  {
+    "revision": "c60117af8cb7b4f2f316",
+    "url": "/pcbdis/static/css/main.70b193d7.chunk.css"
+  },
+  {
+    "revision": "f939f4d358c9fdb359b6",
+    "url": "/pcbdis/static/js/2.a8cecaa5.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/pcbdis/static/js/2.a8cecaa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c60117af8cb7b4f2f316",
+    "url": "/pcbdis/static/js/main.8cc51230.chunk.js"
+  },
+  {
+    "revision": "3fa5909d22191d841365",
+    "url": "/pcbdis/static/js/runtime-main.0f31411e.js"
+  },
+  {
+    "revision": "fbf7000a94010d872c8d1aaccbb54223",
+    "url": "/pcbdis/static/media/Aggash_Sivasothy_Automated PCB Disassembler.fbf7000a.jpg"
+  },
+  {
+    "revision": "2636ba5e9d7510931436d7ff35273eef",
+    "url": "/pcbdis/static/media/Matthew_Yuen_Automated PCB Disassembler.2636ba5e.jpg"
+  },
+  {
+    "revision": "7b65aa8d808981631ecf0c3d80e18e0c",
+    "url": "/pcbdis/static/media/Prabhdeep_Soni_Automated PCB Disassembler.7b65aa8d.jpg"
+  },
+  {
+    "revision": "9c35d491f97437ad1f892a25e20040f1",
+    "url": "/pcbdis/static/media/Vishvam_Mazumdar_Automated PCB Disassembler.9c35d491.jpg"
+  },
+  {
+    "revision": "118a76344cd297bf79012f94aef7e266",
+    "url": "/pcbdis/static/media/Vishwa_Sheth_Automated PCB Disassembler.118a7634.jpg"
+  },
+  {
+    "revision": "00da2e479007ad36ad7f1818b512178b",
+    "url": "/pcbdis/static/media/about-placeholder.00da2e47.jpg"
+  },
+  {
+    "revision": "44bb26799f0f02bd7b550dec5c5894b8",
+    "url": "/pcbdis/static/media/cad-pic1-log1.44bb2679.png"
+  },
+  {
+    "revision": "7589bca7b11743ae97a46cf324f96c58",
+    "url": "/pcbdis/static/media/cad-pic2-log1.7589bca7.png"
+  },
+  {
+    "revision": "cad4e249bef29cd582c1a79a9f3c8e5b",
+    "url": "/pcbdis/static/media/component-list-log1.cad4e249.png"
+  },
+  {
+    "revision": "84a5a780bd58d450edd8c3ce56f5824c",
+    "url": "/pcbdis/static/media/component-pics-log1.84a5a780.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/pcbdis/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "83688d01802acda331da1ccbde20e85a",
+    "url": "/pcbdis/static/media/demo-1.83688d01.png"
+  },
+  {
+    "revision": "cf43204f8a162c7d602d73326c298c23",
+    "url": "/pcbdis/static/media/demo-2.cf43204f.png"
+  },
+  {
+    "revision": "efafbd0ad37b579c4b62e2fdcdf26765",
+    "url": "/pcbdis/static/media/demo-3.efafbd0a.png"
+  },
+  {
+    "revision": "eaae7f353aac4cabfaf7b52da92cf831",
+    "url": "/pcbdis/static/media/demo-4.eaae7f35.png"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/pcbdis/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/pcbdis/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/pcbdis/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/pcbdis/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/pcbdis/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/pcbdis/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/pcbdis/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/pcbdis/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/pcbdis/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/pcbdis/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/pcbdis/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/pcbdis/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "e1e53c62a3a3ff9b641e818195c689b4",
+    "url": "/pcbdis/static/media/pcbdis_pic.e1e53c62.jpg"
+  },
+  {
+    "revision": "8f880ecc85b45eab9c612fedb0adaf0e",
+    "url": "/pcbdis/static/media/poster.8f880ecc.png"
+  },
+  {
+    "revision": "537b0448b46292083aa03ad1bb944873",
+    "url": "/pcbdis/static/media/purchasing-log2.537b0448.png"
+  },
+  {
+    "revision": "0b35153d1f05715b30325c8570e0cf82",
+    "url": "/pcbdis/static/media/purchasing-log3.0b35153d.png"
+  },
+  {
+    "revision": "7f36bb025d96e93909d0b4fff0eb1b53",
+    "url": "/pcbdis/static/media/team-log1.7f36bb02.png"
+  },
+  {
+    "revision": "3c9279ade4b8f8c78f9dbf56f82cc0ce",
+    "url": "/pcbdis/static/media/thumbnail.3c9279ad.jpg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/pcbdis/static/media/video-placeholder.a622fc5d.jpg"
+  },
+  {
+    "revision": "d51160d81b3da2132a96a53b65ff6d78",
+    "url": "/pcbdis/static/media/website-log1.d51160d8.png"
+  }
+]);
